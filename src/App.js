430f9578a5f6393b474931cbc810c2e2836d07e7
Barganskamm <barganskamm@gmail.com>
2020-06-30 11:52:07 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I changed it a lil bit :).
-        </p>
+        <p>I changed it a lil bit :).</p>
         <a
           className="App-link"
           href="https://reactjs.org"
